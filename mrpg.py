460b9d73e161ff536f50495e5cfba380182c9966
Richard <richard@mozor.net>
2012-04-20 16:39:12 -0500

    Changed reference to the project name

--- mrpg.py
+++ mrpg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# IdleRPG 2.0
-# https://github.com/mozor/idlerpg
+# mRPG
+# https://github.com/mozor/mRPG
 #
 # This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 # To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
@@ -13,7 +13,7 @@ import time
 import ConfigParser
 
 config = ConfigParser.RawConfigParser()
-config.read('idlerpg.cfg')
+config.read('mrpg.cfg')
 
 server = config.get('IRC', 'server')
 port = config.getint('IRC', 'port')
@@ -30,7 +30,7 @@ def joinchan(chan):
 ircsock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 ircsock.bind((host, 0))
 ircsock.connect((server, port))
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Mega Python IdleRPG Master\n")
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Mega Python Master\n")
 ircsock.send("NICK "+ botnick +"\n")
 time.sleep(5)
 
